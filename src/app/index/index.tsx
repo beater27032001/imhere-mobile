dd340d605d5368f5583031f28b238de37ef96001
José Carlos <josecarlospaivasantos2001@gmail.com>
2024-10-11 14:47:58 -0300

    documentação + img

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -11,10 +11,14 @@ import { Participant } from "@/src/components/Participant";
 import { styles } from "./styles";
 import { useState } from "react";
 
+// Componente principal da aplicação
 export default function Home() {
+  // Estado para armazenar a lista de participantes
   const [participants, setParticipants] = useState<string[]>([]);
+  // Estado para armazenar o nome do participante atual
   const [participantName, setParticipantName] = useState("");
 
+  // Função para adicionar um participante à lista
   function handleParticipantAdd() {
     if (!participantName) {
       return Alert.alert("Erro", "O nome do participante é obrigatório.");
@@ -27,9 +31,8 @@ export default function Home() {
     setParticipantName("");
   }
 
+  // Função para remover um participante da lista
   function handleParticipantRemove(name: string) {
-    
-
     Alert.alert("Remover", `Deseja remover o participante ${name}?`, [
       { text: "Sim", onPress: () => setParticipants((prevParticipants) => prevParticipants.filter((participant) => participant !== name)) },
       { text: "Não", style: "cancel" },
@@ -38,11 +41,14 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
+      {/* Título do evento */}
       <Text style={styles.eventName}>Nome do evento</Text>
 
+      {/* Data do evento */}
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
 
       <View style={styles.form}>
+        {/* Campo de entrada para o nome do participante */}
         <TextInput
           style={styles.input}
           placeholder="Nome do participante"
@@ -51,11 +57,13 @@ export default function Home() {
           value={participantName}
         />
 
+        {/* Botão para adicionar participante */}
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
 
+      {/* Lista de participantes */}
       <FlatList
         showsVerticalScrollIndicator={false}
         data={participants}
@@ -73,8 +81,6 @@ export default function Home() {
           </Text>
         )}
       />
-
-      
     </View>
   );
 }
